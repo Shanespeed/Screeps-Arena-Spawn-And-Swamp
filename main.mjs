5f2d51c42af880d268236c5b66eca6b47e129c88
ShaneMBOU <shane.woude@kpnmail.nl>
2022-04-28 13:40:40 +0200

    Bug fix and comments added
    Bug fixes:
    - Workers now correctly target containers without getting stuck
    
    New:
    - Comments added
    
    Bugs:
    - Containers that spawn aren't found
    - Workers block tiles of units that are spawning

--- main.mjs
+++ main.mjs
@@ -1,3 +1,5 @@
+// TODO: Divide into seperate files/classes?
+
 import { getObjectsByPrototype, findInRange, findClosestByPath, findClosestByRange } from '/game/utils';
 import { Creep, StructureSpawn, StructureContainer } from '/game/prototypes';
 import { MOVE, TOUGH, ATTACK, RANGED_ATTACK, HEAL, WORK, CARRY, RESOURCE_ENERGY, ERR_NOT_IN_RANGE, ERR_NOT_ENOUGH_RESOURCES } from '/game/constants';
@@ -30,7 +32,8 @@ export function loop() {
         behaviourAssign(workerCreeps[i]);
     }
     
-    // Spawns standard unit bundle first followed by a relentless offense
+    // TODO: attack from both top and bottom side
+    // Spawns standard unit bundle first followed by a continuous offense
     if (isStartOfGame)
     {
         targetLines = new Visual(0, true);
@@ -49,6 +52,7 @@ export function loop() {
     }
 }
 
+// Updates the variables containing the game state.
 function updateState()
 {
     allyCreeps = getObjectsByPrototype(Creep).filter(i => i.my);
@@ -60,9 +64,10 @@ function updateState()
     combatCreeps = attackCreeps.concat(rangedCreeps, healCreeps);
     spawner = getObjectsByPrototype(StructureSpawn).find(i => i.my);
     enemySpawner = getObjectsByPrototype(StructureSpawn).find(i => !i.my);
-    containers = getObjectsByPrototype(StructureContainer).filter(i => i.store.getCapacity(RESOURCE_ENERGY) > 0);
+    containers = getObjectsByPrototype(StructureContainer).filter(i => i.store.getUsedCapacity(RESOURCE_ENERGY) > 0);
 }
 
+// Assigns behaviour to creep's role.
 function behaviourAssign(creep)
 {
     var role = creep.body.find(i => i.type == ATTACK || i.type == RANGED_ATTACK || i.type == HEAL || i.type == WORK).type;
@@ -86,9 +91,9 @@ function behaviourAssign(creep)
     }
 }
 
+// Spawns 5 workers. Builds first platoon (3 attackers, 3 rangers, 1 healer).
 function startGameSpawn()
 {
-    // Spawns 3 workers, 3 attackers, 3 ranger, 1 healer
     if (workerCreeps.length < 5)
         spawnWorker();
     else if (attackCreeps.length < 3)
@@ -100,54 +105,60 @@ function startGameSpawn()
     else
     {
         firstPlatoon = attackCreeps.concat(rangedCreeps, healCreeps);
-        console.log("AUTOBOTS, ROLLOUT!");
         isStartOfGame = false;
     }
 }
 
+// Spawns a fast attacker followed by a fast ranger.
 function buildOffense()
 {
     spawnFastAttacker();
     spawnFastRanger();
 }
 
+// Spawns an attacker.
 function spawnAttacker()
 {
-    if (spawner.store.getCapacity(RESOURCE_ENERGY) >= 140)
-        spawner.spawnCreep([TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, MOVE, MOVE, ATTACK]);
+    spawner.spawnCreep([TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, MOVE, MOVE, ATTACK]);
 }
 
+// Spawns a fast attacker.
 function spawnFastAttacker()
 {
-    if (spawner.store.getCapacity(RESOURCE_ENERGY) >= 190)
-        spawner.spawnCreep([TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, MOVE, MOVE, MOVE, ATTACK]);
+    spawner.spawnCreep([TOUGH, TOUGH, TOUGH, TOUGH, TOUGH, MOVE, MOVE, MOVE, ATTACK]);
 }
 
+// Spawns a ranger.
 function spawnRanger()
 {
-    if (spawner.store.getCapacity(RESOURCE_ENERGY) >= 200)
-        spawner.spawnCreep([TOUGH, TOUGH, MOVE, MOVE, RANGED_ATTACK, RANGED_ATTACK, RANGED_ATTACK]);
+    spawner.spawnCreep([TOUGH, TOUGH, MOVE, MOVE, RANGED_ATTACK, RANGED_ATTACK, RANGED_ATTACK]);
 }
 
+// Spawns a fast ranger.
 function spawnFastRanger()
 {
-    if (spawner.store.getCapacity(RESOURCE_ENERGY) >= 200)
-        spawner.spawnCreep([TOUGH, TOUGH, MOVE, MOVE, MOVE, RANGED_ATTACK, RANGED_ATTACK, RANGED_ATTACK]);
+    spawner.spawnCreep([TOUGH, TOUGH, MOVE, MOVE, MOVE, RANGED_ATTACK, RANGED_ATTACK, RANGED_ATTACK]);
 }
 
+// Spawns a healer.
 function spawnHealer()
 {
-    if (spawner.store.getCapacity(RESOURCE_ENERGY) >= 200)
-        spawner.spawnCreep([TOUGH, MOVE, MOVE, MOVE, MOVE,HEAL]);
+    spawner.spawnCreep([TOUGH, MOVE, MOVE, MOVE, MOVE,HEAL]);
 }
 
+// Spawns a worker.
 function spawnWorker()
 {
-    if (spawner.store.getCapacity(RESOURCE_ENERGY) >= 200)
-        spawner.spawnCreep([MOVE, WORK, CARRY]);
+    spawner.spawnCreep([MOVE, WORK, CARRY]);
 }
 
 // TODO: target healers first if in range
+/* Command creep to follow the attack behaviour. 
+- Detects enemy creeps within 5 range.
+- Targets closest detected enemy creep.
+- Targets enemy spawner if there is no target creep.
+- Moves towards target if target is not in attack range.
+*/
 function attackBehaviour(creep) 
 {
     if (findInRange(creep, enemyCreeps, 5).length > 0)
@@ -164,6 +175,12 @@ function attackBehaviour(creep)
 
 // TODO: target healers first if in range
 // TODO: if target has lower movement parts start kiting
+/* Command creep to follow the ranged attack behaviour. 
+- Detects enemy creeps within 8 range.
+- Targets closest detected enemy creep.
+- Targets enemy spawner if there is no target creep.
+- Moves towards target if target is not in ranged attack range.
+*/
 function rangedAttackBehaviour(creep)
 {
     if (findInRange(creep, enemyCreeps, 8).length > 0)
@@ -179,6 +196,13 @@ function rangedAttackBehaviour(creep)
 }
 
 // TODO: if route is too long start ranged healing
+/* Command creep to follow the heal behaviour. 
+- Detects all allies who are not at max hits.
+- Targets first injured ally.
+- Moves towards target if target is not in (melee) heal range.
+- Follows first platoon if there is no target.
+- Follows closest ally if first platoon is dead.
+*/
 function healBehaviour(creep)
 {
     var injuredAllies = combatCreeps.filter(i => i.hits < i.hitsMax);
@@ -196,7 +220,15 @@ function healBehaviour(creep)
     }   
 }
 
-// TODO: Workers get stuck looting containers
+// BUG: Workers won't loot containers that spawn
+// BUG: Workers block path of spawning units
+/* Command creep to follow the work behaviour. 
+- Targets closest container.
+- Withdraws energy from target.
+- Moves towards target if target is not in withdraw range.
+- Transfers energy to spawn if inventory is full.
+- Moves towards spawn if spawn is not in transfer range.
+*/
 function workBehaviour(creep)
 {
     var targetContainer = findClosestByPath(creep, containers);
@@ -209,7 +241,7 @@ function workBehaviour(creep)
     }
     else
     {   
-        if (creep.withdraw(targetContainer, RESOURCE_ENERGY, 10) == ERR_NOT_ENOUGH_RESOURCES)
+        if (creep.withdraw(targetContainer, RESOURCE_ENERGY) == ERR_NOT_ENOUGH_RESOURCES)
             targetContainer = containers[0];
 
         if (creep.withdraw(targetContainer, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
