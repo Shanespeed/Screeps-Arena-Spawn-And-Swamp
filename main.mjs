525269a410673408db46cd43a31db2ab20d92aa3
ShaneMBOU <shane.woude@kpnmail.nl>
2022-04-28 10:30:12 +0200

    Code refactor main.mjs
    Refactor changes:
    - roleBehaviour function is split up in role specific functions.
    - roleBehaviour function is renamed to behaviourAssign.
    - Order of roles is now consistent across file. (general -> attack -> ranged attack -> heal -> work)

--- main.mjs
+++ main.mjs
@@ -1,10 +1,10 @@
 import { getObjectsByPrototype, findInRange, findClosestByPath, findClosestByRange } from '/game/utils';
 import { Creep, StructureSpawn, StructureContainer } from '/game/prototypes';
-import { WORK, ATTACK, RANGED_ATTACK, HEAL, MOVE, CARRY, TOUGH, RESOURCE_ENERGY, ERR_NOT_IN_RANGE, ERR_NOT_ENOUGH_RESOURCES } from '/game/constants';
+import { MOVE, TOUGH, ATTACK, RANGED_ATTACK, HEAL, WORK, CARRY, RESOURCE_ENERGY, ERR_NOT_IN_RANGE, ERR_NOT_ENOUGH_RESOURCES } from '/game/constants';
 import { Visual } from '/game';
 import { } from '/arena';
 
-const roles = { WORK, ATTACK, RANGED_ATTACK, HEAL };
+const roles = { ATTACK, RANGED_ATTACK, HEAL, WORK };
 let allyCreeps = { };
 let enemyCreeps = { };
 let workerCreeps = { };
@@ -24,13 +24,13 @@ export function loop() {
     // Updates the variable states
     updateState();
 
-    // Workers harvest nearby source
+    // Command workers
     for (var i = 0; i < workerCreeps.length; i++)
     {
-        roleBehaviour(workerCreeps[i]);
+        behaviourAssign(workerCreeps[i]);
     }
     
-    // Spawns standard unit bundle or relentless offense
+    // Spawns standard unit bundle first followed by a relentless offense
     if (isStartOfGame)
     {
         targetLines = new Visual(0, true);
@@ -44,7 +44,7 @@ export function loop() {
     {  
         for (var i = 0; i < combatCreeps.length; i++)
         {
-            roleBehaviour(combatCreeps[i]);
+            behaviourAssign(combatCreeps[i]);
         }
     }
 }
@@ -61,6 +61,29 @@ function updateState()
     spawner = getObjectsByPrototype(StructureSpawn).find(i => i.my);
     enemySpawner = getObjectsByPrototype(StructureSpawn).find(i => !i.my);
     containers = getObjectsByPrototype(StructureContainer).filter(i => i.store.getCapacity(RESOURCE_ENERGY) > 0);
+}
+
+function behaviourAssign(creep)
+{
+    var role = creep.body.find(i => i.type == ATTACK || i.type == RANGED_ATTACK || i.type == HEAL || i.type == WORK).type;
+    
+    switch(role)
+    {
+        case ATTACK:
+            attackBehaviour(creep);
+            break;
+        case RANGED_ATTACK:
+            rangedAttackBehaviour(creep);
+            break;
+        case HEAL:
+            healBehaviour(creep);
+            break;
+        case WORK:
+            workBehaviour(creep);
+            break;
+        default:
+            console.log("ERROR: " + creep.id + " at X: " + creep.x + " Y: " + creep.y + " doesn't have a valid role.");
+    }
 }
 
 function startGameSpawn()
@@ -86,12 +109,6 @@ function buildOffense()
 {
     spawnFastAttacker();
     spawnFastRanger();
-}
-
-function spawnWorker()
-{
-    if (spawner.store.getCapacity(RESOURCE_ENERGY) >= 200)
-        spawner.spawnCreep([MOVE, WORK, CARRY]);
 }
 
 function spawnAttacker()
@@ -124,79 +141,78 @@ function spawnHealer()
         spawner.spawnCreep([TOUGH, MOVE, MOVE, MOVE, MOVE,HEAL]);
 }
 
-function roleBehaviour(creep) // Divide into functions and rename this one
+function spawnWorker()
 {
-    var role = creep.body.find(i => i.type == WORK || i.type == ATTACK || i.type == RANGED_ATTACK || i.type == HEAL).type;
-    
-    switch(role)
+    if (spawner.store.getCapacity(RESOURCE_ENERGY) >= 200)
+        spawner.spawnCreep([MOVE, WORK, CARRY]);
+}
+
+// TODO: target healers first if in range
+function attackBehaviour(creep) 
+{
+    if (findInRange(creep, enemyCreeps, 5).length > 0)
     {
-        case ATTACK: // target healers first if in range
+        if (creep.attack(findClosestByPath(creep, enemyCreeps)) == ERR_NOT_IN_RANGE)
+            creep.moveTo(findClosestByPath(creep, enemyCreeps)); 
+    }
+    else
+    {
+        if (creep.attack(enemySpawner) == ERR_NOT_IN_RANGE)
+            creep.moveTo(enemySpawner);
+    }
+}
 
-            if (findInRange(creep, enemyCreeps, 5).length > 0)
-            {
-                if (creep.attack(findClosestByPath(creep, enemyCreeps)) == ERR_NOT_IN_RANGE)
-                    creep.moveTo(findClosestByPath(creep, enemyCreeps)); 
-            }
-            else
-            {
-                if (creep.attack(enemySpawner) == ERR_NOT_IN_RANGE)
-                    creep.moveTo(enemySpawner);
-            }
-            break;
-                
-        case RANGED_ATTACK: // if target has lower movement parts start kiting // target healers first if in range
+// TODO: target healers first if in range
+// TODO: if target has lower movement parts start kiting
+function rangedAttackBehaviour(creep)
+{
+    if (findInRange(creep, enemyCreeps, 8).length > 0)
+    {
+        if (creep.rangedAttack(findClosestByPath(creep, enemyCreeps)) == ERR_NOT_IN_RANGE)
+            creep.moveTo(findClosestByPath(creep, enemyCreeps));    
+    }
+    else
+    {
+        if (creep.rangedAttack(enemySpawner) == ERR_NOT_IN_RANGE)
+            creep.moveTo(enemySpawner);
+    }
+}
 
-            if (findInRange(creep, enemyCreeps, 8).length > 0)
-            {
-                if (creep.rangedAttack(findClosestByPath(creep, enemyCreeps)) == ERR_NOT_IN_RANGE)
-                    creep.moveTo(findClosestByPath(creep, enemyCreeps));    
-            }
-            else
-            {
-                if (creep.rangedAttack(enemySpawner) == ERR_NOT_IN_RANGE)
-                    creep.moveTo(enemySpawner);
-            }
-            break;
+// TODO: if route is too long start ranged healing
+function healBehaviour(creep)
+{
+    var injuredAllies = combatCreeps.filter(i => i.hits < i.hitsMax);
 
-        case HEAL:
+    if (injuredAllies.length <= 0)
+    {
+        if (firstPlatoon.filter(i => i.body.type != HEAL).length <= 0)
+            creep.moveTo(findClosestByPath(creep, combatCreeps.filter(i => i != creep)));
+        else
+            creep.moveTo(findClosestByPath(creep, firstPlatoon.filter(i => i != creep)));
+    }
+    else if (creep.heal(injuredAllies[0]) == ERR_NOT_IN_RANGE)
+    {
+        creep.moveTo(injuredAllies[0]);
+    }   
+}
 
-            var injuredAllies = combatCreeps.filter(i => i.hits < i.hitsMax);
+// TODO: Workers get stuck looting containers
+function workBehaviour(creep)
+{
+    var targetContainer = findClosestByPath(creep, containers);
+    //targetLines.line(creep, targetContainer, {color: '#FFFF00'});
 
-            if (injuredAllies.length <= 0)
-            {
-                if (firstPlatoon.filter(i => i.body.type != HEAL).length <= 0)
-                    creep.moveTo(findClosestByPath(creep, combatCreeps.filter(i => i != creep)));
-                else
-                    creep.moveTo(findClosestByPath(creep, firstPlatoon.filter(i => i != creep)));
-            }
-            else if (creep.heal(injuredAllies[0]) == ERR_NOT_IN_RANGE)
-            {
-                creep.moveTo(injuredAllies[0]);
-            }
-            
-            break;
+    if (creep.store.getFreeCapacity() <= 0)
+    {
+        if (creep.transfer(spawner, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
+            creep.moveTo(spawner);
+    }
+    else
+    {   
+        if (creep.withdraw(targetContainer, RESOURCE_ENERGY, 10) == ERR_NOT_ENOUGH_RESOURCES)
+            targetContainer = containers[0];
 
-        case WORK: // Workers get stuck looting containers
-
-            var targetContainer = findClosestByPath(creep, containers);
-            //targetLines.line(creep, targetContainer, {color: '#FFFF00'});
-
-            if (creep.store.getFreeCapacity() <= 0)
-            {
-                if (creep.transfer(spawner, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
-                    creep.moveTo(spawner);
-            }
-            else
-            {   
-                if (creep.withdraw(targetContainer, RESOURCE_ENERGY, 10) == ERR_NOT_ENOUGH_RESOURCES)
-                    targetContainer = containers[0];
-
-                if (creep.withdraw(targetContainer, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
-                    creep.moveTo(targetContainer);
-            }
-            break;
-
-        default:
-            console.log("ERROR: " + creep.id + " doesn't have a valid role");
+        if (creep.withdraw(targetContainer, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE)
+            creep.moveTo(targetContainer);
     }
 }
